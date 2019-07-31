a80474887f0ea2f833bf6b657b4bbe815e988b1c
Steffen <Steffen.knaup@5minds.de>
2019-07-31 14:17:54 +0200

    :recycle: Replace Chokidar with fs

--- src/ISolutionExplorerService.ts
+++ src/ISolutionExplorerService.ts
@@ -3,11 +3,9 @@ import {IDiagram, ISolution} from '@process-engine/solutionexplorer.contracts';
 
 export interface ISolutionExplorerService {
 
-  addFileToWatch(filepath: string): void;
-  removeFileFromWatching(filepath: string): void;
+  watchFile(filepath: string, callback: (path: string) => void): void;
 
-  addFileChangeCallback(callback: (path: string) => void): void;
-  removeFileChangeCallback(callback: (path: string) => void): void;
+  removeFileChangeCallback(filepath: string): void;
 
   /**
    * Prepares the solution explorer service to load a given path specification.
